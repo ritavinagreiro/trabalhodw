b49b8c55f27bf4b31ce75c448e255d3cc2b7f4ff
ritavinagreiro <144913341+ritavinagreiro@users.noreply.github.com>
2025-12-16 22:36:56 +0000

    Delete js directory

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-// Seleciona o botão e o menu
-const btn = document.getElementById('menu-btn');
-const menu = document.getElementById('menu-dropdown');
-
-// Alterna a visibilidade do menu ao clicar
-btn.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
-});
